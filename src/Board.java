99434c6a0970761469930d953153094ca62b5501
Datshin1511 <datshinr2002@gmail.com>
2025-02-27 20:53:46 +0530

    Base files

---
+++ src/Board.java
@@ -0,0 +1,51 @@
+public class Board {
+    private int[][] tiles;
+
+    public Board(int[][] tiles){
+        this.tiles = tiles;
+    }
+
+    public String toString(){
+        // TODO: Complete the function
+        return null;
+    }
+
+    public int dimension(){
+        // TODO: Complete the function
+        return 0;
+    }
+
+    public int hamming(){
+        // TODO: Complete the function
+        return 0;
+    }
+
+    public int manhattan(){
+        // TODO: Complete the function
+        return 0;
+    }
+
+    public boolean isGoal(){
+        // TODO: Complete the function
+     return false;   
+    }
+
+    public boolean equals(Object y){
+        // TODO: Complete the function
+        return false;
+    }
+
+    public Iterable<Board> neighbors(){
+        // TODO: Complete the function
+        return null;
+    }
+
+    public Board twin(){
+        // TODO: Complete the function
+        return null;
+    }
+
+    public static void main(String[] args) {
+        // TODO: Unit testing
+    }
+}
---
+++ src/Solver.java
@@ -0,0 +1,25 @@
+public class Solver {
+    public Solver(Board initial){
+        // TODO: Complete the function
+    }
+
+    public boolean isSolvable(){
+        // TODO: Complete the function
+        return false;
+    }
+
+    public int moves(){
+        // TODO: Complete the function
+        return -1;
+    }
+
+    public Iterable<Board> solution(){
+        // TODO: Complete the function
+        return null;
+    }
+
+    public static void main(String[] args) {
+        // TODO: Unit testing
+    }
+
+}
