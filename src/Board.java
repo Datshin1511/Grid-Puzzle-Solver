fb360dd669bb2353afbca9275a5f1f490620a584
Datshin1511 <datshinr2002@gmail.com>
2025-02-28 16:56:56 +0530

    Code refurbished

--- src/Board.java
+++ src/Board.java
@@ -10,38 +10,38 @@ public class Board {
     private final int hammingScore;
     private final int manhattanScore;
 
-    // PRIVATE FUNCTIONS
     private int[][] copyTiles() {
         int[][] copyArray = new int[size][size];
-        for(int i=0; i<size; i++)
+        for (int i = 0; i < size; i++)
             copyArray[i] = Arrays.copyOf(tiles[i], size);
-         return copyArray;
+        return copyArray;
     }
 
-    private void exchangeBlankTile(Board board, int nR, int nC) {
-        int temp = board.tiles[zeroRow][zeroCol];
-        board.tiles[zeroRow][zeroCol] = board.tiles[nR][nC];
-        board.tiles[nR][nC] = temp;
+    private void exchangeBlankTile(int[][] boardTiles, int nR, int nC) {
+        int temp = boardTiles[zeroRow][zeroCol];
+        boardTiles[zeroRow][zeroCol] = boardTiles[nR][nC];
+        boardTiles[nR][nC] = temp;
     }
-    
-    // PUBLIC FUNCTIONS
+
     public Board(int[][] tiles) {
         this.size = tiles.length;
         this.tiles = new int[size][size];
-        
+
         int manhattan = 0, hamming = 0, zeroR = -1, zeroC = -1;
 
-        for(int i=0; i<size; i++){
-            for(int j=0; j<size; j++){
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
                 this.tiles[i][j] = tiles[i][j];
 
-                if(tiles[i][j] == 0) { zeroR = i; zeroC = j; }
-                else {
+                if (tiles[i][j] == 0) {
+                    zeroR = i;
+                    zeroC = j;
+                } else {
                     int correctRow = (tiles[i][j] - 1) / size;
                     int correctCol = (tiles[i][j] - 1) % size;
 
-                    if(tiles[i][j] != (i * size + j + 1)) hamming++;
-                    manhattan = Math.abs(i - correctRow) + Math.abs(j - correctCol);
+                    if (tiles[i][j] != (i * size + j + 1)) hamming++;
+                    manhattan += Math.abs(i - correctRow) + Math.abs(j - correctCol);
                 }
             }
         }
@@ -50,24 +50,20 @@ public class Board {
         this.hammingScore = hamming;
         this.zeroRow = zeroR;
         this.zeroCol = zeroC;
-
     }
 
     public String toString() {
-        String bluePrint = "";
-        
-        bluePrint += Integer.toString(size);
-        bluePrint += "\n";
+        StringBuilder bluePrint = new StringBuilder();
+        bluePrint.append(size).append("\n");
 
-        for(int i=0; i<size; i++){
-            for(int j=0; j<size; j++){
-                bluePrint += Integer.toString(tiles[i][j]);
-                if(j != size-1) bluePrint += " ";
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                bluePrint.append(tiles[i][j]);
+                if (j != size - 1) bluePrint.append(" ");
             }
-            bluePrint += "\n";
+            bluePrint.append("\n");
         }
-
-        return bluePrint;
+        return bluePrint.toString();
     }
 
     public int dimension() {
@@ -79,68 +75,68 @@ public class Board {
     }
 
     public int manhattan() {
-         return manhattanScore;
+        return manhattanScore;
     }
 
     public boolean isGoal() {
-        return hammingScore == 0 || manhattanScore == 0;   
+        return hammingScore == 0;
     }
 
     public boolean equals(Object y) {
-        return tiles.equals(y);
+        if (this == y) return true;
+        if (!(y instanceof Board)) return false;
+        Board that = (Board) y;
+        return Arrays.deepEquals(this.tiles, that.tiles);
     }
 
     public Board twin() {
-        return new Board(copyTiles());
+        int[][] twinTiles = copyTiles();
+        if (zeroRow == 0) {
+            exchangeBlankTile(twinTiles, 1, 0);
+        } else {
+            exchangeBlankTile(twinTiles, 0, 1);
+        }
+        return new Board(twinTiles);
     }
 
     public Iterable<Board> neighbors() {
         return new Iterable<Board>() {
-
             @Override
             public Iterator<Board> iterator() {
                 return new Iterator<Board>() {
                     private final int[][] directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-                    private final int max = directions.length;
                     private int index = 0;
 
                     public boolean hasNext() {
-                        while (index < max) {
+                        while (index < directions.length) {
                             int nRow = zeroRow + directions[index][0];
                             int nCol = zeroCol + directions[index][1];
-
-                            if(nRow >= 0 && nRow < size && nCol >= 0 && nCol < size) return true;
+                            if (nRow >= 0 && nRow < size && nCol >= 0 && nCol < size) {
+                                return true;
+                            }
                             index++;
                         }
                         return false;
                     }
 
                     public Board next() {
-                        if(!hasNext()){
+                        if (!hasNext()) {
                             throw new ArrayIndexOutOfBoundsException("Array has exhausted");
                         }
-
+                        int[][] newTiles = copyTiles();
                         int nRow = zeroRow + directions[index][0];
-                        int nCol = zeroRow + directions[index][1];
-
-                        Board tempBoard = twin();
-                        exchangeBlankTile(tempBoard, nRow, nCol);
+                        int nCol = zeroCol + directions[index][1];
+                        exchangeBlankTile(newTiles, nRow, nCol);
                         index++;
-
-                        return tempBoard;
-
+                        return new Board(newTiles);
                     }
-    
                 };
             }
-            
         };
     }
 
-    // MAIN - UNIT TESTING
-    public static void main(String[] args) {        
+    public static void main(String[] args) {
         int[][] tiles = {{8, 1, 3}, {4, 0, 2}, {7, 6, 5}};
-
         Board board = new Board(tiles);
 
         StdOut.println("Hamming distance: " + board.hamming());
@@ -149,7 +145,7 @@ public class Board {
         StdOut.println("--------------------------");
         StdOut.println("Current Board: ");
         StdOut.println("--------------------------");
-        StdOut.println(board.toString());
+        StdOut.println(board);
 
         Iterator<Board> it = board.neighbors().iterator();
 
@@ -157,10 +153,9 @@ public class Board {
         StdOut.println("Neighbours");
         StdOut.println("--------------------------");
 
-        while(it.hasNext()){
-            StdOut.println(it.next().toString());
+        while (it.hasNext()) {
+            StdOut.println(it.next());
             StdOut.println();
         }
     }
-
 }
