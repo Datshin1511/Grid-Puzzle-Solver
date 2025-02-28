526c46e70cefa53249963b5b9975da0121200597
Datshin1511 <datshinr2002@gmail.com>
2025-02-28 16:57:37 +0530

    Code corrected

--- src/Solver.java
+++ src/Solver.java
@@ -1,38 +1,110 @@
 import java.util.Iterator;
-
 import edu.princeton.cs.algs4.MinPQ;
+import edu.princeton.cs.algs4.StdOut;
 
 public class Solver {
-    public int minBoardMoves;
-    private MinPQ<Board> boardPossibilites;
+    private int minBoardMoves;
+    private int solutionSize;
+    private Board[] solutionPath;
 
-    public Solver(Board initial){
-        // TODO: Complete the function
-        boardPossibilites.insert(initial);
-        Iterator<Board> it = initial.neighbors();
+    private class SearchNode implements Comparable<SearchNode> {
+        Board board;
+        int moves;
+        SearchNode prev;
+        int priority;
 
-        while(it.hasNext()){
-            it.next();
+        SearchNode(Board board, int moves, SearchNode prev) {
+            this.board = board;
+            this.moves = moves;
+            this.prev = prev;
+            this.priority = board.manhattan() + moves;
         }
-        minBoardMoves = 0;
+
+        @Override
+        public int compareTo(SearchNode o) {
+            return Integer.compare(this.priority, o.priority);
+        }
     }
 
-    public boolean isSolvable(){
-        // TODO: Complete the function
-        return false;
+    // PRIVATE FUNCTIONS
+    private void constructGoalPath(SearchNode node) {
+        solutionSize = node.moves + 1;
+        solutionPath = new Board[solutionSize];
+
+        while (node != null) {
+            solutionPath[--solutionSize] = node.board;
+            node = node.prev;
+        }
+        minBoardMoves = solutionPath.length - 1;
     }
 
-    public int moves(){
-        return minBoardMoves;
+    // PUBLIC FUNCTIONS
+    public Solver(Board initial) {
+        if (initial == null)
+            throw new IllegalArgumentException("Initial board cannot be null");
+
+        this.minBoardMoves = -1;
+        MinPQ<SearchNode> boardPQ = new MinPQ<>();
+        boardPQ.insert(new SearchNode(initial, 0, null));
+
+        while (!boardPQ.isEmpty()) {
+            SearchNode currentNode = boardPQ.delMin();
+
+            if (currentNode.board.isGoal()) {
+                constructGoalPath(currentNode);
+                return;
+            }
+
+            for (Board neighbour : currentNode.board.neighbors()) {
+                if (currentNode.prev == null || !neighbour.equals(currentNode.prev.board)) {
+                    boardPQ.insert(new SearchNode(neighbour, currentNode.moves + 1, currentNode));
+                }
+            }
+        }
     }
 
-    public Iterable<Board> solution(){
-        // TODO: Complete the function
-        return null;
+    public int moves() {
+        return isSolvable() ? minBoardMoves : -1;
     }
 
+    public boolean isSolvable() {
+        return solutionPath != null;
+    }
+
+    public Iterable<Board> solution() {
+        if (!isSolvable()) return null;
+        return new Iterable<Board>() {
+            @Override
+            public Iterator<Board> iterator() {
+                return new Iterator<Board>() {
+                    private int index = 0;
+                    
+                    public boolean hasNext() {
+                        return index < solutionPath.length;
+                    }
+
+                    public Board next() {
+                        if (!hasNext())
+                            throw new ArrayIndexOutOfBoundsException("Array has exhausted");
+                        return solutionPath[index++];
+                    }
+                };
+            }
+        };
+    }
+
+    // MAIN - UNIT TESTING
     public static void main(String[] args) {
-        // TODO: Unit testing
-    }
+        int[][] tiles = {{0, 1, 3}, {4, 2, 5}, {7, 8, 6}};
+        Board board = new Board(tiles);
+        Solver solver = new Solver(board);
 
+        StdOut.println("Is the board solvable ?:  " + (solver.isSolvable() ? "Yes" : "No"));
+
+        for(Board sol: solver.solution()) {
+            StdOut.println(sol.toString());
+        }
+
+        StdOut.println("Minimum moves required to achieve goal position: " + solver.minBoardMoves);
+    }
 }
